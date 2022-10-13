599437ff9e4891bb205b00d8129a1f7520d421c3
Anthony Chablov <achablov@myseneca.ca>
2022-10-12 23:27:09 -0400

    fixed for bug

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -12,21 +12,21 @@ const Form = () => {
                             you want to run past me.
                         </p>
 
-                        <form class="contact-form">
-                            <div class="contact-form__field">
-                                <input id="name" class="input-text js-input" type="text" required/>
-                                <label class="label" for="name">Name*</label>
+                        <form className="contact-form">
+                            <div className="contact-form__field">
+                                <input id="name" className="input-text js-input" type="text" required/>
+                                <label className="label" htmlFor="name">Name*</label>
                             </div>
-                            <div class="contact-form__field">
-                                <input id="email" class="input-text js-input" type="email" required/>
-                                <label class="label" for="email">E-mail*</label>
+                            <div className="contact-form__field">
+                                <input id="email" className="input-text js-input" type="email" required/>
+                                <label className="label" htmlFor="email">E-mail*</label>
                             </div>
-                            <div class="contact-form__field">
-                                <input id="message" class="input-text js-input" type="text" required/>
-                                <label class="label" for="message">Message*</label>
+                            <div className="contact-form__field">
+                                <input id="message" className="input-text js-input" type="text" required/>
+                                <label className="label" htmlFor="message">Message*</label>
                             </div>
-                            <div class="contact-form__field contact-form__field--align">
-                                <input class="submit-btn" type="submit" value="Send Message"/>
+                            <div className="contact-form__field contact-form__field--align">
+                                <input className="submit-btn" type="submit" value="Send Message"/>
                             </div>
                         </form>
                         
