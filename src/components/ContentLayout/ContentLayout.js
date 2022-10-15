82a09def4905d2cb7d43763fff17a2d93a7afad8
Anthony Chablov <achablov@myseneca.ca>
2022-10-15 14:18:08 -0400

    create skills section

--- src/components/ContentLayout/ContentLayout.js
+++ src/components/ContentLayout/ContentLayout.js
@@ -1,11 +1,12 @@
 import ContentSectionText from "../ContentSectionText/ContentSectionText";
 import ContentSectionProjects from "../ContentSectionProjects/ContentSectionProjects";
+import ContentSectionSkills from "../ContentSectionSkills/ContentSectionSkills";
 const ContentLayout = () => {
   return (
     <div className="content-layout">
         <ContentSectionText bgColorClass={'content-section--white'} insertClass={'content-section'}/>
         <ContentSectionProjects/>
-
+        <ContentSectionSkills/>
     </div>
   )
 }
---
+++ src/components/ContentSectionSkills/ContentSectionSkills.js
@@ -0,0 +1,13 @@
+const ContentSectionSkills = () => {
+  return (
+    <div className='content-section-skills'>
+        <div className="content-section-skills__wrapper">
+            <div className="container">
+                My Skills
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ContentSectionSkills
