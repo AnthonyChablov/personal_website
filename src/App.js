f74a596cf151478b4044d9cadcafeb9fb026378f
Anthony Chablov <achablov@myseneca.ca>
2022-10-26 12:33:29 -0400

    add dot ring cursor

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import {
     BrowserRouter as Router
 } from 'react-router-dom';
 import AnimatedRoutes from './components/AnimatedRoutes/AnimatedRoutes';
-
+import Cursor from './components/Cursor/Cursor'; 
 import NavBar from './components/Navbar/Navbar';
 
 function App() {
     return (
-        <Router>   
+        <Router>
+            <Cursor/>   
             <NavBar/>
             <AnimatedRoutes/>
             
