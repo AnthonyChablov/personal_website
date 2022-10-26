31bc9db7feb11abbc18560c82a8a0e7042772bac
Anthony Chablov <achablov@myseneca.ca>
2022-10-26 12:08:54 -0400

    update about

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,13 +3,14 @@ import Header from '../components/Header/Header';
 import Hero from '../components/Hero/Hero';
 import ContentSectionText from '../components/ContentSectionText/ContentSectionText';
 import ContentSectionEducation from '../components/ContentSectionEducation/ContentSectionEducation';
+import ContentSectionSkills from '../components/ContentSectionSkills/ContentSectionSkills';
 const About = () => {
   return (
     <>
-        
         <Hero heroType={'About'}/>
         <ContentSectionText contentType={'aboutText'} bgColorClass={'content-section--white'} insertClass={'content-section'}/>
         <ContentSectionEducation/>
+        <ContentSectionSkills/>
         <Footer/>
     </>
   )
